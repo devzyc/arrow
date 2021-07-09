33adb49d65a0d6f0c57a4ca5362679d3a7e752d4
zengyongchang <zeng_yong_chang@163.com>
2021-07-09 17:37:24 +0800

    remove the wrong wrapper class(RecyclerViewExt_class)

--- arrow/src/main/java/com/zyc/arrow/putbypackage/androidx/recyclerview/widget/RecyclerViewExt.kt
+++ arrow/src/main/java/com/zyc/arrow/putbypackage/androidx/recyclerview/widget/RecyclerViewExt.kt
@@ -7,19 +7,17 @@ import androidx.recyclerview.widget.RecyclerView
 /**
  * @author zeng_yong_chang@163.com
  */
-class RecyclerViewExt {
 
-  fun RecyclerView.click(position: Int, @IdRes clickableAreaResId: Int = 0) {
-    postDelayed({
-      val viewHolder = findViewHolderForAdapterPosition(position)
-      if (viewHolder == null) {
-        Toast.makeText(context, "切换列表项失败", Toast.LENGTH_SHORT)
-          .show()
-      } else {
-        (if (clickableAreaResId == 0) viewHolder.itemView
-        else viewHolder.itemView.findViewById(clickableAreaResId))
-          .performClick()
-      }
-    }, 500)
-  }
+fun RecyclerView.click(position: Int, @IdRes clickableAreaResId: Int = 0) {
+  postDelayed({
+    val viewHolder = findViewHolderForAdapterPosition(position)
+    if (viewHolder == null) {
+      Toast.makeText(context, "切换列表项失败", Toast.LENGTH_SHORT)
+        .show()
+    } else {
+      (if (clickableAreaResId == 0) viewHolder.itemView
+      else viewHolder.itemView.findViewById(clickableAreaResId))
+        .performClick()
+    }
+  }, 500)
 }
