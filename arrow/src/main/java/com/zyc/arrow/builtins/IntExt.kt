2283112508ffc7fbeef542f6456ea4120f1e56c6
zengyongchang <zeng_yong_chang@163.com>
2022-01-25 17:04:14 +0800

    little

--- arrow/src/main/java/com/zyc/arrow/builtins/IntExt.kt
+++ arrow/src/main/java/com/zyc/arrow/builtins/IntExt.kt
@@ -2,5 +2,9 @@ package com.zyc.arrow.builtins
 
 import android.content.res.Resources
 
+/**
+ * @author zeng_yong_chang@163.com
+ */
+
 val Int.dp: Int
     get() = (this * Resources.getSystem().displayMetrics.density).toInt()
