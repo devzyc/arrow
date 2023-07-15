5cbf1dcd6024abd7a33a7f98e74e171a4e9f7e56
zengyongchang <zeng_yong_chang@163.com>
2023-07-15 11:58:08 +0800

    use reified to deserialize gson

--- arrow/src/main/java/com/zyc/arrow/JsonFileUtil.kt
+++ arrow/src/main/java/com/zyc/arrow/JsonFileUtil.kt
@@ -8,6 +8,7 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonIOException
 import com.google.gson.JsonSyntaxException
+import com.google.gson.reflect.TypeToken
 import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonWriter
 import java.io.File
@@ -21,43 +22,54 @@ import java.lang.reflect.Type
  */
 object JsonFileUtil {
 
-  private const val TAG = "JsonFileUtil"
+    const val TAG = "JsonFileUtil"
 
-  private fun cacheDir(context: Context): File {
-    return context.cacheDir
-  }
-
-  private fun getFile(name: String, context: Context): File {
-    return File(cacheDir(context), name)
-  }
-
-  fun <T> loadObject(classOfT: Class<T>, fileName: String, context: Context): T? {
-    var obj: T? = null
-    try {
-      val reader = JsonReader(FileReader(getFile(fileName, context)))
-      obj = Gson().fromJson(reader, classOfT)
-      reader.close()
-    } catch (e: IOException) {
-      Log.e(TAG, String.format("failed on reading json from file: %s", fileName), e)
-    } catch (e: JsonIOException) {
-      Log.e(TAG, String.format("failed on reading json from file: %s", fileName), e)
-    } catch (e: JsonSyntaxException) {
-      Log.e(TAG, String.format("failed on reading json from file: %s", fileName), e)
+    private fun cacheDir(context: Context): File {
+        return context.cacheDir
     }
-    return obj
-  }
 
-  fun saveToJson(src: Any, typeOfSrc: Type, fileName: String, context: Context) {
-    val writer: JsonWriter
-    try {
-      writer = JsonWriter(FileWriter(getFile(fileName, context), false))
-      writer.setIndent("  ")
-      GsonBuilder().setPrettyPrinting()
-        .create()
-        .toJson(src, typeOfSrc, writer)
-      writer.close()
-    } catch (e: IOException) {
-      Log.e(TAG, String.format("failed on writing json to file: %s", fileName), e)
+    fun getFile(
+        name: String,
+        context: Context
+    ): File {
+        return File(cacheDir(context), name)
     }
-  }
+
+    inline fun <reified T> loadObject(
+        fileName: String,
+        context: Context
+    ): T? {
+        var obj: T? = null
+        try {
+            val reader = JsonReader(FileReader(getFile(fileName, context)))
+            obj = Gson().fromJson(reader, object : TypeToken<T>() {}.type)
+            reader.close()
+        } catch (e: IOException) {
+            Log.e(TAG, String.format("failed on reading json from file: %s", fileName), e)
+        } catch (e: JsonIOException) {
+            Log.e(TAG, String.format("failed on reading json from file: %s", fileName), e)
+        } catch (e: JsonSyntaxException) {
+            Log.e(TAG, String.format("failed on reading json from file: %s", fileName), e)
+        }
+        return obj
+    }
+
+    fun saveToJson(
+        src: Any,
+        typeOfSrc: Type,
+        fileName: String,
+        context: Context
+    ) {
+        val writer: JsonWriter
+        try {
+            writer = JsonWriter(FileWriter(getFile(fileName, context), false))
+            writer.setIndent("  ")
+            GsonBuilder().setPrettyPrinting()
+                .create()
+                .toJson(src, typeOfSrc, writer)
+            writer.close()
+        } catch (e: IOException) {
+            Log.e(TAG, String.format("failed on writing json to file: %s", fileName), e)
+        }
+    }
 }
